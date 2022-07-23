a40ea8e19d43b957b665d6ccc1560e6f2b1538bd
Nguyễn Tiến Dũng <88418919+dung195@users.noreply.github.com>
2022-07-23 21:16:11 +0700

    mới

--- LCA.cpp
+++ LCA.cpp
@@ -1,77 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAXN 100005
-int n, h[MAXN], p[MAXN][20], par[MAXN];
-
-
-void dfs(int u){
-	if(h[u]>0) return;
-	if(par[u] == 0) h[u] = 1;
-	else{
-		dfs(par[u]);
-		h[u] = h[par[u]] + 1;
-	}
+#define ll long long
+#define ld long double
+#define pq priority_queue
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define bs binary_search
+const ll sized = 1e6;
+const ll N = 1e7;
+const ll inf = 1e18;
+const ll MOD = 123456789;       
+const ll LOG = 20;
+int m,n,q;
+vector<int>graph[sized];
+int h[sized];
+int p[sized][LOG];
+void dfs(int S){
+    for(int i=0;i<graph[S].size();i++){
+        p[graph[S][i]][0]=S;
+        h[graph[S][i]]=h[S]+1;                                      
+        dfs(graph[S][i]);
+    }
 }
-int getbit(int x, int i){
-	return (x>>i)&1;
+int getbit(int x,int i){                                
+    return (x>>i)&1;
 }
-int lca(int u, int v){
-	if(h[u]<h[v]) swap(u,v);
-	//u la dinh co do sau lon hon v => nhay u
-	//B1: nhay u sao cho h[u] = h[v]
-	int x = h[u] - h[v];
-
-	for(int i = 0; i<20; i++)
-		if(getbit(x,i)==1){
-			u = p[u][i];
-		}
-	if(u==v) return u;
-	for(int i = 19; i>=0; i--){
-		if(p[u][i] != p[v][i]){
-			u = p[u][i];
-			v = p[v][i];
-		}
-	}
-	return p[u][0];
+int lca(int a,int b){
+    if(h[a]<h[b]) swap(a,b);
+    int x=h[a]-h[b];
+    for(int i=LOG-1;i>=0;i--){
+        if(getbit(x,i)==1) a=p[a][i];
+    }
+    if(a==b) return a;
+    for(int i=LOG-1;i>=0;i--){
+        if(p[a][i]!=p[b][i]){
+            a=p[a][i];
+            b=p[b][i];
+        }
+    }
+    return p[a][0];
 }
-int main() {
-	int test;
-	cin>>test;
-	for(int te = 1; te<=test; te++){
-		int u,v,x;
-		
-		cin>>n;
-		for(int i = 1; i<=n; i++){
-			cin>>x;
-			while(x--){
-				cin>>u;
-				par[u] = i;
-			}
-		}
-		
-		//Root la dinh ko co par
-		for(int i = 1; i<=n; i++)
-			dfs(i);
-		for(int i = 1; i<=n; i++)
-			p[i][0] = par[i];
-
-		//Tinh p
-		for(int i = 1; i<20; i++){
-			for(int u = 1; u<=n; u++)
-				p[u][i] = p[p[u][i-1]][i-1];
-		}
-	
-		cout<<"Case "<<te<<":"<<endl;
-		cin>>x;
-		while(x--){
-			cin>>u>>v;
-			cout<<lca(u,v)<<endl;
-		}
-		for(int i = 0; i<=n; i++){
-			h[i] = 0;
-			par[i] = 0;
-		}
-		
-	}
-	return 0;
+main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("main.inp","r",stdin);
+	// freopen("main.out","w",stdout);
+    cin>>n>>q;
+    for(int i=1;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        graph[a].push_back(b);              
+    }
+    dfs(1);
+    for(int i=1;i<LOG;i++){
+        for(int u=1;u<=n;u++){
+            p[u][i]=p[p[u][i-1]][i-1]; // cha thứ 2**i của x =cha thứ 2**(i-1) của cha thứ 2**(i-1) của x
+        }
+    }
+    for(int i=0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<lca(a,b)<<endl;
+    }
+    return 0;
 }
